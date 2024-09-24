055cb8ffd5df335834cf8dfa6e45be7f50d8b5da
João Vinezof <joaovitorvinezof@gmail.com>
2024-09-24 12:27:53 -0300

    feat: game base screen

--- app/(tabs)/game.tsx
+++ app/(tabs)/game.tsx
@@ -3,17 +3,25 @@ import { StyleSheet } from 'react-native';
 import Button from '@/components/Button';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useState } from 'react';
+
+type moveOptions = 'rock' | 'paper' | 'scissor' | null;
 
 export default function HomeScreen() {
+  const [moveSelected, setMoveSelected] = useState<moveOptions>(null);
+
   return (
     <ThemedView style={styles.container}>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="subtitle">Choose your move</ThemedText>
       </ThemedView>
       <ThemedView style={styles.section}>
-        <Button title="✊ Rock" onPress={() => {}} />
-        <Button title="👋 Paper" onPress={() => {}} />
-        <Button title="✌️ Scissor" onPress={() => {}} />
+        <Button color={moveSelected === 'rock' ? '#051937' : undefined} title="✊ Rock" onPress={() => setMoveSelected('rock')} />
+        <Button color={moveSelected === 'paper' ? '#051937' : undefined} title="👋 Paper" onPress={() => setMoveSelected('paper')} />
+        <Button color={moveSelected === 'scissor' ? '#051937' : undefined} title="✌️ Scissor" onPress={() => setMoveSelected('scissor')} />
+      </ThemedView>
+      <ThemedView style={styles.confirmSection}>
+        <Button title="Confirm" onPress={() => { }} />
       </ThemedView>
     </ThemedView>
   );
@@ -37,4 +45,9 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     gap: 8
   },
+  confirmSection: {},
+  selectedButton: {
+    backgroundColor: '#051937'
+  },
+  button: {},
 });
